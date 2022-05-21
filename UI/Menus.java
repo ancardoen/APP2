e237a9e0930f95875dfdc9f3a41cbb258e88d48d
Antonio Cardoen <ancardoen@alumnos.uai.cl>
2022-05-21 19:33:33 -0400

    Update Menus.java

--- UI/Menus.java
+++ UI/Menus.java
@@ -5,15 +5,18 @@ public class Menus {
 
     public void mostrar_menu(){
         System.out.println("Menu de opciones: ");
-        System.out.println("1) Mostrar base de datos");
-        System.out.println("2) Agregar un nuevo libro");
-        System.out.println("3) Quitar un libro");
-        System.out.println("4) Editar un libro");
-        System.out.println("5) Agregar o quitar una sede");
-        System.out.println("6) Agregar o quitar una seccion");
-        System.out.println("7) Agregar o quitar un piso");
-        System.out.println("8) Buscar un libro");
-        System.out.println("9) Salir  del programa");
+        System.out.println("1)  Mostrar base de datos");
+        System.out.println("2)  Agregar un nuevo libro");
+        System.out.println("3)  Quitar un libro");
+        System.out.println("4)  Editar un libro");
+        System.out.println("5)  Agregar una sede");
+        System.out.println("6)  Quitar una sede");
+        System.out.println("7)  Agregar una seccion");
+        System.out.println("8)  Quitar una seccion");
+        System.out.println("9)  Agregar un piso");
+        System.out.println("10) Quitar un piso");
+        System.out.println("11) Buscar un libro");
+        System.out.println("12) Salir  del programa");
     }
 
     public void ejecutar(int opcion){
@@ -54,6 +57,17 @@ public class Menus {
                     break;
                 case 9:
 
+                    flag = false;
+                    break;
+                case 10:
+
+                    flag = false;
+                    break;
+                case 11:
+
+                    flag = false;
+                    break;
+                case 12:
                     flag = false;
                     break;
                 default:
