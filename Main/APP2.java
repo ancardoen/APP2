f1bf77074e191ead63161cba5ec13279271633b0
Antonio Cardoen <ancardoen@alumnos.uai.cl>
2022-05-24 11:45:57 -0400

    main

--- Main/APP2.java
+++ Main/APP2.java
@@ -5,13 +5,14 @@ import Managers.FileManager;
 import Managers.DataManager;
 import UI.Menus;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class APP2 {
     public static void main(String[] args) throws FileNotFoundException, CSVNotFoundException {
         //Setear los datos
-        private File theFile = "inventario.csv";
+        String theFile = "inventario.csv";
         FileManager f = new FileManager(theFile);
         DataManager.getInstance().setData(f.getData());
         //Mostrar el menu
